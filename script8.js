20f6b0d70a1043c093c19414d5d536db3f13ec67
fabaneon <fabaneon@gmail.com>
2022-10-06 23:53:04 +0900

    2022-10-06 Wave 만들기 공부
    좌표계로만 표현하기엔 한계가 있다.
    일단 삼각함수를 먼저 배워야겠다.

---
+++ script8.js
@@ -0,0 +1,34 @@
+
+// function setup(){
+//   createCanvas(windowWidth, windowHeight);
+//   background(255)
+//   frameRate(12)
+// }
+
+function setup() {
+  createCanvas(1000,1000);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+}
+function draw(){
+  background (10,20,30);
+  noFill();
+  stroke(0,128,230,50);
+  translate(width / 2, height/2);
+
+  for(var i=0; i< 200; i++){
+    push(); 
+
+    rotate(sin(frameCount + i * 2 ) * 50);
+    // 색 입히는건 카피임.
+    var r = map(sin(frameCount), -1, 1, 50, 255);
+    var g = map(cos(frameCount /2), -1, 1, 50, 255);
+    var b = map(tan(frameCount /4), -1, 1, 50, 255);
+
+    stroke(r,g,b);
+
+    rect(0 ,0 ,600-i*3 ,600-i*3 , 200-i);
+
+    pop();
+  }
+}
