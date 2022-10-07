74aa38636f320be0bb72adc0be17171b36d32777
fabaneon <fabaneon@gmail.com>
2022-10-07 23:44:40 +0900

    2022-10-07 sin 파도 생성
    하지만 실용성은 zero에 가깝다.
    이제 단순히 선을 그리는것이 아닌
    점과 점을 연결하고 각 점들을 다른 index로 지정하여 서로 다른 y값을 갖도록 만들어봐야겠다.

--- script9.js
+++ script9.js
@@ -9,32 +9,22 @@ function draw(){
   background (10,20,30);
   noFill();
   stroke(0,128,230,50);
-  translate(400, height/2);
+  translate(width/2, height/2);
   for(var i=0; i< windowWidth; i++){
     stroke(sin(frameCount)*i,cos(frameCount/4)*i,tan(frameCount/2)+i,0+(i*i));
 
     push();
     // fill(0,125,230,10)
     if(mouseIsPressed === true){
-      ellipse(i,sin(frameCount + i * 0.5 ) * windowHeight/4,50,50)
-      ellipse(-i,sin(frameCount + i * 0.5 ) * windowHeight/4,50,50)
-
-    }
-    else{
       line(i, sin(frameCount + i * 0.5 ) * windowHeight/4,i,  500)
       line(-i, sin(frameCount + i * 0.5 ) * windowHeight/4, -i,  500)
 
     }
+    else{
+      ellipse(i,sin(frameCount + i * 0.5 ) * windowHeight/4,50,50)
+      ellipse(-i,sin(frameCount + i * 0.5 ) * windowHeight/4,50,50)
 
-
-    // 색 입히는건 카피임.
-    // var g = map(cos(frameCount /2), -1, 1, 50, 255);
-    // var b = map(tan(frameCount /4), -1, 1, 50, 255);
-    //
-    // var r = sin(frameCount), -1, 1, 50, 255);
-
-    // rect(0 ,0 ,600-i*3 ,600-i*3 , 200-i);
-
+    }
     pop();
   }
   }
