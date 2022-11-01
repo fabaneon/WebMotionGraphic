d701a3e81a536b09c686729dc9ad49d314967f11
fabaneon <fabaneon@gmail.com>
2022-11-01 14:49:45 +0000

    2022-11-01 Drag 이벤트 공부 음.. 일단 드래그 이벤트 호출 메소드는 알아냈는데.. 문제는 객체에 마우스 이동수치만큼 어떻게 그 값을 더하느냐이다.
    더하는 수식은 당연히 만들었지만
    그 변화 기준수치를 어떻게 잡을지 조금 고민이다.
    
    - 참고로 보았던 영상
    https://www.youtube.com/watch?v=7PYvx8u_9Sk

--- HTML5canvas/practice/previewing2/canvas.js
+++ HTML5canvas/practice/previewing2/canvas.js
@@ -3,20 +3,53 @@ var canvas = document.querySelector("canvas");
 	canvas.width= window.innerWidth;
 	canvas.height = window.innerHeight;
 
-
-var mouse = {
+let mouse = {
 	x: undefined,
-	y: undefined
+	y: undefined,
+	clicked: false,
+	startx: undefined,
+	starty: undefined,
+	prevx: undefined,
+	prevy: undefined,
+	
+	down: function(event) {
+	event.preventDefault();
+	mouse.clicked = true;
+	
+	mouse.startx = event.clientX - canvas.offsetLeft + window.scrollX;	
+	mouse.starty = event.clientX - canvas.offsetLeft + window.scrollX;	
+	mouse.prevx = 0;
+	mouse.prevy = 0;
+		
+	console.log("mouse clicked  " + mouse.clicked);
+	},
+	
+	up: function(event) {
+	event.preventDefault();
+	mouse.prevx = 0;
+	mouse.prevy = 0;
+	mouse.clicked = false;
+	console.log("mouse clicked  " + mouse.clicked);
+	}
 }
-window.addEventListener("resize", 
-	function(){
-	canvas.width = window.innerWidth+300;
-	canvas.height = window.innerHeight;
-});
+
+canvas.onmousedown = mouse.down;
+canvas.onmouseup = mouse.up;
+
 window.addEventListener("mousemove",
     function(event){
+				console.log(mouse.prevx +" / "+ mouse.prevy);
         mouse.x = event.clientX - canvas.offsetLeft + window.scrollX;
         mouse.y = event.clientY - canvas.offsetTop + window.scrollY;
+		if(mouse.clicked){
+			mouse.prevx = mouse.x - mouse.startx;
+			mouse.prevy = mouse.y - mouse.starty;
+			mouse.startx = mouse.x;
+			mouse.starty = mouse.y;
+
+
+			// console.log(mouse.startx + " / " + mouse.x);
+		}
         // console.log(mouse.x +" / "+mouse.y);
     }
 )
@@ -24,17 +57,79 @@ window.addEventListener("mousemove",
 var ctx = canvas.getContext('2d');
 
 function point(x,y) {
-	var x = this.x;
-	var y = this.y;
+	this.x = x;
+	this.y = y;
 }
 
+function CreateRect(x,y,vx,vy,size,r,g,b){
+	this.x = x;
+	this.y = y;
+	this.vx = vx;
+	this.vy = vy;
+	this.size = size;
+	
+	
+	this.draw = function(){
+		ctx.beginPath();
+		
+		ctx.moveTo(this.x-this.size,this.y+this.size);
+		
+		ctx.lineTo(this.x+this.size,this.y+this.size);
+		ctx.lineTo(this.x+this.size,this.y-this.size);
+		ctx.lineTo(this.x-this.size,this.y-this.size);
+		ctx.lineTo(this.x-this.size,this.y+this.size);
+
+		ctx.lineWidth = 1.5;
+		ctx.strokeStyle = "rgba("+r+","+g+","+b+",0.9)"
+		ctx.stroke();
+		ctx.fillStyle = "rgba("+r+","+g+","+b+",0.5)"
+		ctx.fill();
+	}
+	this.update = function(){
+		if (mouse.x > this.x-this.size && mouse.x < this.x + this.size &&
+			mouse.y > this.y-this.size && mouse.y < this.y + this.size){
+			console.log("mouse in "+r+"");
+			if(mouse.clicked){
+				console.log(mouse.prevx +" / "+ mouse.prevy);
+				this.x += mouse.prevx;
+				this.y += mouse.prevy;
+			}
+		}
+		
+		
+		this.draw();
+	}
+}
+
+let RectArr = [];
+
+function init(){
+	RectArr = [];
+	for(var i=0; i < 2; i++){
+		var size = 50; 
+		console.log(canvas.width +" / "+ canvas.height);
+		var x = Math.floor(Math.random() * (window.innerWidth - size*2));
+		var y = Math.floor(Math.random() * (window.innerHeight - size*2));
+		
+		var vx = (Math.random() * 0.5) * 0.8;
+		var vy = (Math.random() * 0.5) * 0.8;
+		
+		var r = Math.floor(Math.random() * 255);
+		var g = Math.floor(Math.random() * 255);
+		var b = Math.floor(Math.random() * 255);
+		
+		RectArr.push(new CreateRect(x,y,vx,vy,size,r,g,b));
+	}
+}
+init();
 
 function animate(){
 	requestAnimationFrame(animate);
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	
-	
-	
+
+	RectArr.forEach((Rect,index)=>{
+		Rect.update();
+	})
 	
 	ctx.fillStyle = "skyblue"
 
@@ -48,3 +143,11 @@ function animate(){
 }
 
 animate();
+	
+	
+window.addEventListener("resize", 
+	function(){
+	canvas.width = window.innerWidth+300;
+	canvas.height = window.innerHeight;
+	init();
+});
