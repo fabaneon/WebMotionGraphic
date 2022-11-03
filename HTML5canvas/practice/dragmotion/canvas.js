c9326f9b0b704323b97f36f2c5e1337fb0e3e70d
fabaneon <fabaneon@gmail.com>
2022-11-03 13:41:37 +0000

    2022-11-03 드래그 예제 완성 mouse down 이벤트 발생때의 마우스 상대좌표를 startxy 좌표값으로 설정하는바람에 클릭을 하고있지 않을동안의 공백차가 발생하였었다. 그것때문에 prevXY(지긍은 movedXY) 값이 갑자기 미쳐날뛰었던것. 지금은 상시 업데이트로 바꿔주었고 이를 통해 문제를 해결하였다. 자세한건 커밋내용 확인.
    15 ~ 50줄쯤 일꺼다.

--- HTML5canvas/practice/dragmotion/canvas.js
+++ HTML5canvas/practice/dragmotion/canvas.js
@@ -4,33 +4,32 @@ var canvas = document.querySelector("canvas");
 	canvas.height = window.innerHeight;
 
 let mouse = {
-	x: undefined,
-	y: undefined,
+	x: null,
+	y: null,
 	clicked: false,
-	startx: undefined,
-	starty: undefined,
-	prevx: undefined,
-	prevy: undefined,
+	startx: null,
+	starty: null,
+	movedx: null,
+	movedy: null,
 	
 	down: function(event) {
-	event.preventDefault();
-	mouse.clicked = true;
-	
-	mouse.startx = event.clientX - canvas.offsetLeft + window.scrollX;	
-	mouse.starty = event.clientX - canvas.offsetLeft + window.scrollX;	
-	mouse.prevx = 0;
-	mouse.prevy = 0;
-		
-	console.log("mouse clicked  " + mouse.clicked);
+		event.preventDefault();
+		mouse.movedx = 0;
+		mouse.movedy = 0;
+		mouse.clicked = true;
+		console.log("mouse clicked  " + mouse.clicked);
 	},
 	
 	up: function(event) {
-	event.preventDefault();
-	mouse.prevx = 0;
-	mouse.prevy = 0;
-	mouse.clicked = false;
-	console.log("mouse clicked  " + mouse.clicked);
-	}
+		event.preventDefault();
+		mouse.movedx = 0;
+		mouse.movedy = 0;
+		mouse.clicked = false;
+		console.log("mouse clicked  " + mouse.clicked);
+	},
+	
+	
+	
 }
 
 canvas.onmousedown = mouse.down;
@@ -38,19 +37,15 @@ canvas.onmouseup = mouse.up;
 
 window.addEventListener("mousemove",
     function(event){
-				console.log(mouse.prevx +" / "+ mouse.prevy);
         mouse.x = event.clientX - canvas.offsetLeft + window.scrollX;
         mouse.y = event.clientY - canvas.offsetTop + window.scrollY;
+		mouse.movedx = mouse.x - mouse.startx;
+		mouse.movedy = mouse.y - mouse.starty;
+		mouse.startx = mouse.x;
+		mouse.starty = mouse.y;
 		if(mouse.clicked){
-			mouse.prevx = mouse.x - mouse.startx;
-			mouse.prevy = mouse.y - mouse.starty;
-			mouse.startx = mouse.x;
-			mouse.starty = mouse.y;
-
-
-			// console.log(mouse.startx + " / " + mouse.x);
+			console.log(mouse.x +" / "+ mouse.movedx +" / "+ mouse.movedy+" / "+ mouse.y);
 		}
-        // console.log(mouse.x +" / "+mouse.y);
     }
 )
 
@@ -60,7 +55,7 @@ function point(x,y) {
 	this.x = x;
 	this.y = y;
 }
-
+/** 사각형 객체를 생성,갱신하는 묶음 함수 */
 function CreateRect(x,y,vx,vy,size,r,g,b){
 	this.x = x;
 	this.y = y;
@@ -85,14 +80,15 @@ function CreateRect(x,y,vx,vy,size,r,g,b){
 		ctx.fillStyle = "rgba("+r+","+g+","+b+",0.5)"
 		ctx.fill();
 	}
+
 	this.update = function(){
 		if (mouse.x > this.x-this.size && mouse.x < this.x + this.size &&
 			mouse.y > this.y-this.size && mouse.y < this.y + this.size){
-			console.log("mouse in "+r+"");
+			// console.log("mouse in "+r+"");
 			if(mouse.clicked){
-				console.log(mouse.prevx +" / "+ mouse.prevy);
-				this.x += mouse.prevx;
-				this.y += mouse.prevy;
+					this.x += mouse.movedx;
+					this.y += mouse.movedy;
+					console.log(this.x + "/"+ this.y);
 			}
 		}
 		
@@ -134,12 +130,16 @@ function animate(){
 	ctx.fillStyle = "skyblue"
 
 	ctx.font = "bold 24px Arial";
-	ctx.fillText("복습", 100,120);    
+	ctx.fillText("드래그 모션", 100,120);    
 
 	ctx.font = "bold 12px Arial";
-	ctx.fillText("시발 다 까먹은게 좆같네", 100,200);    
+	ctx.fillText("Shape객체를 마우스로 드래그하여 이동시키는 기본 예제이다", 100,200);    
+	ctx.fillText("아직 마우스의 속도를 완벽히 따라가진 못하지만 드래그앤드롭은 충분히 된다.", 100,240);    
 
-	ctx.fillText("자세한것은 F12 -> Source || 주석 확인",100,240);  
+	ctx.fillText("상시 업데이트되는 마우스의 현재값과 이전값의 차를 구하여",100,280);  
+	ctx.fillText("객체 범위 안에 들어가있을 경우 해당 값을 중앙xy 좌표에 더해준다.",100,320);  
+	ctx.fillText("자세한것은 F12 -> Source || 주석 확인",100,360);  
+
 }
 
 animate();
