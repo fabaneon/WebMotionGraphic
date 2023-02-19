441a0a890fe644849c20c6723cb5ef95b21556ab
fabaneon <fabaneon@gmail.com>
2023-02-19 08:17:32 +0000

    2023-02-19 난관봉착과 실마리 획득
    음.. 우선 내가 원하는건 폰트에 있는.
    정확히는 폰트의 모양대로 색상이 입혀진 부분, 픽셀의 좌표값들을 얻고
    이것으로 내가 원하는 그림을 그리는것이다.
    
    여기서 첫번째 힌트를 얻은 인스턴스속성이 getImageData였고
    
    이중 getImageData.data 노드를 통해
    픽셀 배열같이 보이는것을 발견할 수 있었다.
    
    하지만 이해는 여전히 하지 못한 상태이고
    
    이것으로는 어떠한 효율적인 작업도 하지 못한다.
    
    다음 실마리는 PIXIJS라는 라이브러리이다.
    
    D3.JS와 비슷한 성향의 2D WebGL API인데
    몹시 빠르고 특히 이미지에 대한것을 많이 다룬다고 한다.
    
    시도해봐야지.

--- HTML5canvas/Typography/typo1/canvas.js
+++ HTML5canvas/Typography/typo1/canvas.js
@@ -10,38 +10,90 @@ const viewYCenter = canvas.height/5*0.8
 function Text(str,x,y,density) {
 	
 	this.fontWidth = canvas.width/2;
-	this.fontSize = this.fontWidth*1.2;
+	this.fontSize = 400;
 	
-	this.stageWidth = canvas.width/2;
-	this.stageHeight = canvas.height/2;
-
+	this.stageWidth = canvas.width;
+	this.stageHeight = canvas.height;
+	this.dotPos = function(fontX,fontY,fontTop,fontBtm,fontWidth){
+			const imageData = ctx.getImageData(fontX,
+								   fontY-fontTop,
+									   fontX+fontWidth,
+									   fontY+fontBtm).data;
+		const particles = [];
+		let i =0;
+		let width = 0;
+		let pixel;
+		for(let height = 0; height < this.stageHeight; height += density){
+			++i;
+			const slide = (i%2) ==0;
+			width = 0;
+			if(slide == 1){
+				width +=6;
+			}
+			for(width; width < this.stageWidth; width+= density){
+				pixel = imageData[((width +(height * this.stageWidth))*4)-1];
+				if(pixel !=0 && width > 0 && width < this.stageWidth && height > 0  && height< this.stageHeight){
+					particles.push({
+						x: width,
+						y: height,
+					});
+				}
+			}
+		}
+		return particles;
+	}
 	
 	this.draw = function() {
 
-		ctx.clearRect(x,y,canvas.width,canvas.height);
+		ctx.clearRect(0,0,canvas.width,canvas.height);
 		
 		ctx.font = 'bold '+this.fontSize+'px aria';
 		ctx.fillStyle = 'rgba(0,0,0,0.3)';
 		ctx.textBaseline = 'middle';
 		const fontPos = ctx.measureText(str);
-		ctx.fillText(str,this.stageWidth,this.stageHeight);
 		const fontTop = fontPos.actualBoundingBoxAscent;
 		const fontBtm = fontPos.actualBoundingBoxDescent;
-		const fontSize = fontPos.width;
-		//ctx.textAlign = 'center';
-		for (var i = this.stageWidth; i < this.stageWidth+fontSize; i+=5){
-			ctx.fontWidth = 0.1;
-			ctx.moveTo(i,this.stageHeight-fontTop);
-			ctx.lineTo(i,this.stageHeight+fontBtm);
-			ctx.strokeStyle = 'rgba(0,100,200,1)';
-			ctx.stroke();
-			//console.log(i);
+		const fontWidth = fontPos.width;
+		const fontX = (this.stageWidth - fontPos.width)/2;
+		const fontY = (this.stageHeight - this.fontSize)/2+fontTop+fontBtm;
+		ctx.textAlign = 'start';
+		ctx.fillText(str,fontX, fontY);
+
+		var dotPoint = this.dotPos(fontX,fontY,fontTop,fontBtm,fontWidth);
+		// ctx.fontWidth = 0.1;
+		// ctx.strokeStyle = 'rgba(0,100,200,1)';
+
+		// for (var i = fontX; i < fontX+fontWidth; i+=density){
+		// ctx.beginPath();
+		// ctx.arc(i,fontY-fontTop,5,0,Math.PI*2,false);
+		// ctx.arc(i,fontY+fontBtm,5,0,Math.PI*2,false);
+		// ctx.stroke();
+		// }
+		
+		// ctx.beginPath();
+		// ctx.strokeStyle = 'black';
+		// ctx.arc(fontX,fontY-fontTop,5,0,Math.PI*2,false);
+		// ctx.stroke()
+
+		for(var check = 0; check < dotPoint.length; check++){
+				var dotX = dotPoint[check].x;
+				var dotY = dotPoint[check].y;
+				
+				ctx.beginPath();
+				ctx.strokeStyle = 'red';
+				ctx.arc(dotX,dotY,5,0,Math.PI*2,false);
+				ctx.stroke()
 		}
-			
+
+		
+		// ctx.beginPath();
+		// ctx.strokeStyle = 'pink';
+		// ctx.arc(fontX,fontY+fontBtm,5,0,Math.PI*2,false);
+		// ctx.stroke()
 	}
 }
 
-const drawing = new Text('F',viewXCenter,viewYCenter,50);
+const drawing = new Text('g',viewXCenter,viewYCenter,50);
 
 function draw(){
 	requestAnimationFrame(draw);
