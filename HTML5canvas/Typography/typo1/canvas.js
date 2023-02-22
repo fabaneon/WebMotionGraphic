0cbf31c16ff4971031c61099592fb8556dbe4b1a
fabaneon <fabaneon@gmail.com>
2023-02-22 14:29:37 +0000

    2023-02-22 sibal nom eu getImageData.
    아.. 왜 fillText의 rgb를 인식하질 못할까..

--- HTML5canvas/Typography/typo1/canvas.js
+++ HTML5canvas/Typography/typo1/canvas.js
@@ -5,39 +5,57 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
-
+var show = null;
+var voke = null;
 function Text(str,density) {
 	
 	this.fontWidth = canvas.width/2;
-	this.fontSize = 400;
+	this.fontSize = 300;
 	
 	this.stageWidth = canvas.width;
 	this.stageHeight = canvas.height;
 	this.dotPos = function(fontX,fontY,fontTop,fontBtm,fontWidth){
+            
 			const imageData = ctx.getImageData(fontX,
 								   fontY-fontTop,
 									   fontX+fontWidth,
-									   fontY+fontBtm).data;
+									   fontTop+fontBtm);
+        voke = imageData;
+        
+        imageData[0] = 255;
+        imageData[4] = 255;
+        imageData[8] = 255;
+        imageData[16] = 255;
+        imageData[20] = 255;
 		const particles = [];
 		let pixel;
-		for(var i = 0; i < imageData.length;i+=4){
-				imageData[i] = 255 - imageData[i];
-                imageData[i+1] = 255 - imageData[i+1];
-                imageData[i+2] = 255 - imageData[i+2];
+        let i = 0;
+        const density = 15;
+        const arrDen = density*4;
+        // for(var x1 = fontX; x1 < fontX+fontWidth; x1+=density){
+        //     for(var y1 = fontY-fontTop; y1 < fontY+fontBtm; y1+=density){
+        //                 var r = imageData[i];
+        //                 var g = imageData[i+1];
+        //                 var b = imageData[i+2];
 
-				var pixel1 = imageData[i];
-                var pixel2 = imageData[i+1];
-                var pixel3 = imageData[i+2];
-                var pixel4 = imageData[i+3];
-		        
-                particles.push({
-                   r: pixel1, 
-                   g: pixel2, 
-                   b: pixel3, 
-                   a: pixel4, 
-                });
-        }
-        return particles
+        //             if(r !== 255 && g !== 255 && b !== 255){                        
+        //                 particles.push({
+        //                     x: x1,
+        //                     y: y1,
+        //                     r: r,
+        //                     g: g,
+        //                     b: b,
+        //                 });
+        //             i+=arrDen;
+                    
+                    
+        //         }
+                
+        //     }
+            
+        // }
+        ctx.putImageData(imageData,fontX,fontY-fontTop);
+        return particles;
         
 		
 	}
@@ -45,22 +63,28 @@ function Text(str,density) {
 	this.draw = function() {
 
 		ctx.clearRect(0,0,canvas.width,canvas.height);
-		
+
+
+        
 		ctx.font = 'bold '+this.fontSize+'px aria';
-		ctx.fillStyle = 'rgba(0,0,0,0.3)';
-		ctx.textBaseline = 'middle';
+		ctx.fillStyle = 'blue';
 		const fontPos = ctx.measureText(str);
 		const fontTop = fontPos.actualBoundingBoxAscent;
 		const fontBtm = fontPos.actualBoundingBoxDescent;
 		const fontWidth = fontPos.width;
 		const fontX = (this.stageWidth - fontPos.width)/2;
 		const fontY = (this.stageHeight - this.fontSize)/2+fontTop+fontBtm;
-		ctx.textAlign = 'start';
-		ctx.fillText(str,fontX, fontY);
 
-		var dotPoint = this.dotPos(fontX,fontY,fontTop,fontBtm,fontWidth);
-        console.log(dotPoint[1]);
-		// ctx.fontWidth = 0.1;
+        ctx.textBaseline = 'top';
+        //ctx.textAlign = 'start';
+		ctx.fillText(str,fontX, fontY-fontBtm);
+
+        
+        //ctx.fillRect(200,200,350,350);
+        var dotPoint = this.dotPos(fontX,fontY,fontTop,fontBtm,fontWidth);
+        show = dotPoint;
+        
+        // ctx.fontWidth = 0.1;
 		// ctx.strokeStyle = 'rgba(0,100,200,1)';
 
 		// for (var i = fontX; i < fontX+fontWidth; i+=density){
@@ -74,21 +98,21 @@ function Text(str,density) {
 		// ctx.strokeStyle = 'black';
 		// ctx.arc(fontX,fontY-fontTop,5,0,Math.PI*2,false);
 		// ctx.stroke()
-        for(var x = fontX; x < fontWidth; x+=10){
-            for(var check = 0,y = fontY-fontTop; check < dotPoint.length; check++, y+=10){
-                    var r = dotPoint[check].r;
-                    var g = dotPoint[check].g;
-                    var b = dotPoint[check].b;
-                    var a = dotPoint[check].a;
+            // for(var i = 0; i < dotPoint.length; i++){
+            //         var r = '200';
+            //         var g = '200';
+            //         var b = '200';
+            //         var a = '1';
 
-                    ctx.beginPath();
-                    ctx.fillStyle = 'rgba('+r+','+g+','+b+','+a+')';
-                    ctx.strokeStyle = 'rgba('+r+','+g+','+b+','+a+')';
-                    ctx.arc(x,y,1,0,Math.PI*2,false);
-                    ctx.fill();
-                    ctx.stroke()
-            }
-        }
+                    
+                
+            //         ctx.beginPath();
+            //         ctx.fillStyle = 'red';
+            //         ctx.strokeStyle = 'rgba('+r+','+g+','+b+','+a+')';
+            //         ctx.arc(dotPoint[i].x,dotPoint[i].y,2,0,Math.PI*2,false);
+            //         ctx.fill();
+            //         ctx.stroke()
+            // }
 
 		
 		// ctx.beginPath();
@@ -106,3 +130,10 @@ function draw(){
 	drawing.draw();
 }
 draw();
+window.addEventListener('mousemove',
+                       function(event){
+            event.preventDefault();
+            console.log(show);
+            console.log(voke);
+
+})
